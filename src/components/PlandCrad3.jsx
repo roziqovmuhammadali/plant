81a219a072f4f247e0c94ee9e295149a8f2882a3
RoziqovM <mroziqov494@gmail.com>
2024-04-07 06:33:46 +0500

    commit

--- src/components/PlandCrad3.jsx
+++ src/components/PlandCrad3.jsx
@@ -1,149 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Search as SearchIcon,
   ShoppingCart as ShoppingCartIcon,
 } from "@mui/icons-material";
 import IconCheckboxes from "./Chack";
 
-import image5 from "../../public/image  5.jpg";
-import image6 from "../../public/image  6.jpg";
-import image7 from "../../public/image  7.jpg";
-import image8 from "../../public/image  8.jpg";
-import image9 from "../../public/image  9.jpg";
-
 function PlantCard3() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/plants")
+      .then((res) => res.json())
+      .then((data) => setTrips(data));
+  }, []);
   return (
     <>
-      <div className="card group relative space-y-3 pb-[50px]">
-        <div className="relative h-[260px] w-[258px] bg-[#FBFBFB] p-1 group-hover:bg-[#F0F0F0]">
-          <div className="h-[250px] w-[250px] px-1">
-            <img
-              src={image5}
-              alt="logo"
-              className="h-full w-full object-cover"
-            />
+      {trips.slice(4, 11).map((trip) => (
+        <div className="card group relative space-y-3 pb-[50px]">
+          <div className="relative h-[260px] w-[258px] bg-[#FBFBFB] p-1 group-hover:bg-[#F0F0F0]">
+            <div className="h-[250px] w-[250px] px-1">
+              <img
+                src={trip.images}
+                alt="logo"
+                className="h-full w-full object-cover"
+              />
+            </div>
+            <div className=" relative bottom-36 left-0 flex h-full w-full items-center justify-center opacity-0 group-hover:opacity-100">
+              <span className="text-[#46A358]">
+                <ShoppingCartIcon />
+              </span>
+              <span className="text-[#46A358]">
+                <IconCheckboxes />
+              </span>
+              <SearchIcon />
+            </div>
           </div>
-          <div className=" relative bottom-36 left-0 flex h-full w-full items-center justify-center opacity-0 group-hover:opacity-100">
-            <span className="text-[#46A358]">
-              <ShoppingCartIcon />
-            </span>
-            <span className="text-[#46A358]">
-              <IconCheckboxes />
-            </span>
-            <SearchIcon />
-          </div>
+          <h1 className="text-[16px] font-normal leading-4 text-[#3D3D3D]">
+            {trip.title}
+          </h1>
+          <p className="text-[18px] font-semibold leading-4 text-[#46A358]">
+            ${trip.price}
+          </p>
         </div>
-        <h1 className="text-[16px] font-normal leading-4 text-[#3D3D3D]">
-          Blushing Bromeliad
-        </h1>
-        <p className="text-[18px] font-semibold leading-4 text-[#46A358]">
-          $139.00
-        </p>
-      </div>
-      <div className="card group relative space-y-3 pb-[50px]">
-        <div className="relative h-[260px] w-[258px] bg-[#FBFBFB] p-1 group-hover:bg-[#F0F0F0]">
-          <div className="h-[250px] w-[250px] px-1">
-            <img
-              src={image6}
-              alt="logo"
-              className="h-full w-full object-cover"
-            />
-          </div>
-          <div className=" relative bottom-36 left-0 flex h-full w-full items-center justify-center opacity-0 group-hover:opacity-100">
-            <span className="text-[#46A358]">
-              <ShoppingCartIcon />
-            </span>
-            <span className="text-[#46A358]">
-              <IconCheckboxes />
-            </span>
-            <SearchIcon />
-          </div>
-        </div>
-        <h1 className="text-[16px] font-normal leading-4 text-[#3D3D3D]">
-          Aluminum Plant
-        </h1>
-        <p className="text-[18px] font-semibold leading-4 text-[#46A358]">
-          $179.00
-        </p>
-      </div>
-      <div className="card group relative space-y-3 pb-[50px]">
-        <div className="relative h-[260px] w-[258px] bg-[#FBFBFB] p-1 group-hover:bg-[#F0F0F0]">
-          <div className="h-[250px] w-[250px] px-1">
-            <img
-              src={image7}
-              alt="logo"
-              className="h-full w-full object-cover"
-            />
-          </div>
-          <div className=" relative bottom-36 left-0 flex h-full w-full items-center justify-center opacity-0 group-hover:opacity-100">
-            <span className="text-[#46A358]">
-              <ShoppingCartIcon />
-            </span>
-            <span className="text-[#46A358]">
-              <IconCheckboxes />
-            </span>
-            <SearchIcon />
-          </div>
-        </div>
-        <h1 className="text-[16px] font-normal leading-4 text-[#3D3D3D]">
-          Bird's Nest Fern
-        </h1>
-        <p className="text-[18px] font-semibold leading-4 text-[#46A358]">
-          $99.00
-        </p>
-      </div>
-      <div className="card group relative space-y-3 pb-[50px]">
-        <div className="relative h-[260px] w-[258px] bg-[#FBFBFB] p-1 group-hover:bg-[#F0F0F0]">
-          <div className="h-[250px] w-[250px] px-1">
-            <img
-              src={image8}
-              alt="logo"
-              className="h-full w-full object-cover"
-            />
-          </div>
-          <div className=" relative bottom-36 left-0 flex h-full w-full items-center justify-center opacity-0 group-hover:opacity-100">
-            <span className="text-[#46A358]">
-              <ShoppingCartIcon />
-            </span>
-            <span className="text-[#46A358]">
-              <IconCheckboxes />
-            </span>
-            <SearchIcon />
-          </div>
-        </div>
-        <h1 className="text-[16px] font-normal leading-4 text-[#3D3D3D]">
-          Broadleaf Lady Palm
-        </h1>
-        <p className="text-[18px] font-semibold leading-4 text-[#46A358]">
-          $59.00
-        </p>
-      </div>
-      <div className="card group relative space-y-3 pb-[50px]">
-        <div className="relative h-[260px] w-[258px] bg-[#FBFBFB] p-1 group-hover:bg-[#F0F0F0]">
-          <div className="h-[250px] w-[250px] px-1">
-            <img
-              src={image9}
-              alt="logo"
-              className="h-full w-full object-cover"
-            />
-          </div>
-          <div className=" relative bottom-36 left-0 flex h-full w-full items-center justify-center opacity-0 group-hover:opacity-100">
-            <span className="text-[#46A358]">
-              <ShoppingCartIcon />
-            </span>
-            <span className="text-[#46A358]">
-              <IconCheckboxes />
-            </span>
-            <SearchIcon />
-          </div>
-        </div>
-        <h1 className="text-[16px] font-normal leading-4 text-[#3D3D3D]">
-          Chinese Evergreen
-        </h1>
-        <p className="text-[18px] font-semibold leading-4 text-[#46A358]">
-          $39.00
-        </p>
-      </div>
+      ))}
     </>
   );
 }
